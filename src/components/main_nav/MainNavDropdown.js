3196814282073820e002bf509fd17c59fa2b1f73
Atay Sultangaziev <ataysultangaziev@MacBook-Pro-Atay.local>
2019-11-24 21:36:30 +0600

    asdf

--- src/components/main_nav/MainNavDropdown.js
+++ src/components/main_nav/MainNavDropdown.js
@@ -2,40 +2,69 @@ import React from "react";
 import right from "../../static/images/general/arrowright.png";
 
 const MainNavDropdown = () => (
-    <div className="main-nav__dropdown">
-        <div className="main-nav-col">
-            <ul className="main-nav-dropdown__list">
-                <li className="main-nav-dropdown__item">
-                    <a href="" className="main-nav-dropdown__link">
-                    <span className="main-nav-dropdown__text">
+    <div className="main-nav__dropdown-wrapper">
+        <div className="main-nav__dropdown">
+            <div className="main-nav__col">
+                <ul className="main-subnav__list">
+                    <li className="main-subnav__item">
+                        <a href="" className="main-subnav__link">
+                    <span className="main-subnav__text">
                         Взрослые самокаты
                     </span>
-                        <img src={right} className="main-nav-dropdown__icon"/>
-                    </a>
-                    <ul className="main-subnav__list">
-
-                    </ul>
-                </li>
-                <li className="main-nav-dropdown__item">
-                    <a href="" className="main-nav-dropdown__link">
-                                <span className="main-nav-dropdown__text">
+                            <img src={right} className="main-subnav__icon"/>
+                        </a>
+                        <ul className="main-subnav__list">
+                            <li className="main-subnav__item">
+                                <a href="" className="main-subnav__link">
+                                <span className="main-subnav__text">
+                                    Детские двухколесные самокаты
+                                </span>
+                                </a>
+                            </li>
+                            <li className="main-subnav__item">
+                                <a href="" className="main-subnav__link">
+                                <span className="main-subnav__text">
+                                    Детские трехколесные самокаты
+                                </span>
+                                </a>
+                            </li>
+                            <li className="main-subnav__item">
+                                <a href="" className="main-subnav__link">
+                                <span className="main-subnav__text">
+                                    Детские самокаты с большими колесами
+                                </span>
+                                </a>
+                            </li>
+                            <li className="main-subnav__item">
+                                <a href="" className="main-subnav__link">
+                                <span className="main-subnav__text">
+                                    Детские самокаты с надувными колесами
+                                </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="main-subnav__item">
+                        <a href="" className="main-subnav__link">
+                                <span className="main-subnav__text">
                                     Детские самокаты
                                 </span>
-                        <img src={right} className="main-nav-dropdown__icon"/>
-                    </a>
-                </li>
-                <li className="main-nav-dropdown__item">
-                    <a href="" className="main-nav-dropdown__link">
-                    <span className="main-nav-dropdown__text">
+                            <img src={right} className="main-subnav__icon"/>
+                        </a>
+                    </li>
+                    <li className="main-subnav__item">
+                        <a href="" className="main-subnav__link">
+                    <span className="main-subnav__text">
                         Городские самокаты
                     </span>
-                        <img src={right} className="main-nav-dropdown__icon"/>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div className="main-nav-col">
+                            <img src={right} className="main-subnav__icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="main-nav__col">
 
+            </div>
         </div>
     </div>
 );
